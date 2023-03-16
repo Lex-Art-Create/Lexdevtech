94abfa2de920f9f36c4ec475b314ac1e8ef8d75d
Lexdevcreate99 <107650364+Lex-Art-Create@users.noreply.github.com>
2023-03-15 23:51:07 -0700

    Add files via upload

---
+++ assets/js/JSForm.js
@@ -0,0 +1,69 @@
+const paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+function payWithPaystack(e) {
+  e.preventDefault();
+
+  let handler = PaystackPop.setup({
+    key: 'pk_test_xxxxxxxxxx', // Replace with your public key
+    email: document.getElementById("email-address").value,
+    amount: document.getElementById("amount").value * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+
+  handler.openIframe();
+}
+
+var paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener('submit', payWithPaystack, false);
+function payWithPaystack() {
+  var handler = PaystackPop.setup({
+    key: 'YOUR_PUBLIC_KEY', // Replace with your public key
+    email: document.getElementById('email-address').value,
+    amount: document.getElementById('amount').value * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
+    currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
+    ref: 'YOUR_REFERENCE', // Replace with a reference you generated
+    callback: function(response) {
+      //this happens after the payment is completed successfully
+      var reference = response.reference;
+      alert('Payment complete! Reference: ' + reference);
+      // Make an AJAX call to your server with the reference to verify the transaction
+    },
+    onClose: function() {
+      alert('Transaction was not completed, window closed.');
+    },
+  });
+  handler.openIframe();
+}
+
+callback: function(response){
+  $.ajax({
+    url: 'http://www.yoururl.com/verify_transaction?reference='+ response.reference,
+    method: 'get',
+    success: function (response) {
+      // the transaction status is in response.data.status
+    }
+  });
+}
+
+callback: function(response) {
+  window.location = "http://www.yoururl.com/verify_transaction.php?reference=" + response.reference;
+};
+// On the redirected page, you can call Paystack's verify endpoint.
+
+{
+  "amount": 34,000, 
+  "email": "customer@email.com",
+  "currency": "NGN",
+  "mobile_money": {
+    "phone" : "0553241149",
+    "provider" : "MTN"
+  }
+}
